90d9333b88edf7f582ec8ebee823cf6b03f182e2
Grzegorz Milka <grzegorzmilka@gmail.com>
2020-05-01 15:13:51 +0200

    Implement the image scraper.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# 200-wichtigste-deutsche-Adjektive
-A collection of tools for download and processing adjective-pictures from deutschlernerblog.de.
+# 200 wichtigsten deutschen Adjektive
+
+A collection of tools for downloading and processing adjective-pictures from
+deutschlernerblog.de.
+
+## Overview
+
+This repo contains one executable package `tools.scrape` that downloads images
+from deutschlernerblog.de
+
+## Background
+
+I use [Anki](https://apps.ankiweb.net/) flashcards to learn languages. I noticed
+that deutschlernerblog.de has a collection of 100 images representing
+adjectives, and I thought that they would make for good flashcards: the images
+look good and placing opposite words side-by-side makes it easy to create a
+clear flashcard.
---
+++ tools/scrape.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""An executable package that scrapes adjective images from deutschlernerblog.
+
+This binary scrapes adjective images from deutschlernenblog and saves them to
+images/ folder.
+"""
+import logging
+import re
+from typing import List
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def extract_filename_from_image_url(url: str):
+    """
+    >>> extract_filename_from_image_url('https://deutschlernerblog.de/wp-content/uploads/2017/07/wild_zahm_Adjektive_Deutsch_deutschlernerblog.png')
+    'wild_zahm_Adjektive_Deutsch_deutschlernerblog.png'
+    """
+    return re.compile(r'([^/]+)$').search(url)[1]
+
+
+def scrape_images_and_metadata_from_site(url: str) -> List:
+    logging.info('Scraping image sources from: ' + url)
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text)
+
+    imgs = soup.findAll('img')
+    imgs = [
+        img for img in imgs if img.has_attr('width') and img['width'] == '640'
+        and img.has_attr('height') and img['height'] == '402'
+        and not img.has_attr('data-lazy-src')
+    ]
+    srcs = [img['src'] for img in imgs]
+
+    weiter = soup.find(string=re.compile('weiter\s+$'))
+    next_link = weiter.next['href'] if weiter else None
+
+    return (srcs, next_link)
+
+
+def download_image(url: str) -> bytes:
+    logging.info('Downloading image: ' + extract_filename_from_image_url(url))
+    image = requests.get(url)
+    image.raise_for_status()
+    return image.content
+
+
+def fetch_image_sources() -> List:
+    WEBSITE_PART_1_URL = "https://deutschlernerblog.de/die-200-wichtigsten-deutschen-adjektive-mit-bildern-lernen-teil-1/"
+    url = WEBSITE_PART_1_URL
+    all_srcs = []
+    while True:
+        srcs, next_link = scrape_images_and_metadata_from_site(url)
+        all_srcs.extend(srcs)
+        if not next_link:
+            break
+        url = next_link
+    return all_srcs
+
+
+def main():
+    logging.basicConfig(filename='scrape.log',level=logging.INFO)
+    image_srcs = fetch_image_sources()
+    metadata = []
+    for img_url in image_srcs:
+        fn = extract_filename_from_image_url(img_url)
+        with open('images/' + fn, 'wb') as f:
+            f.write(download_image(img_url))
+
+if __name__ == "__main__":
+    main()
