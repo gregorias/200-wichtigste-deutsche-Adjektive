aae8eac01d595abc6c63e58a5d9c59def6e54b1d
Grzegorz Milka <grzegorzmilka@gmail.com>
2020-05-18 10:20:44 +0200

    TODO add pic manipulation

--- tools/process.py
+++ tools/process.py
@@ -3,6 +3,56 @@
 """A collection of utilities for processing scraped images."""
 from typing import Tuple
 
+from PIL import Image
+
+
+def get_picture_size(pic_filename: str) -> Tuple[int, int]:
+    with Image.open(pic_filename) as im:
+        return im.size
+
+
+class AdjectivePic:
+    def __init__(self, filename: str, adjective: str, subtitle: bool):
+        self.filename = filename
+        self.adjective = adjective
+        self.subtitle = subtitle
+
+    def get_size(self) -> Tuple[int, int]:
+        return get_picture_size(self.filename)
+
+
+def remove_subtitle_from_adjective_pic(pic: AdjectivePic) -> AdjectivePic:
+    if not pic.subtitle:
+        return pic
+    raise Exception("Not Implemented")
+
+
+class DoubleAdjectivePic:
+    def __init__(self, filename: str, subtitles: bool):
+        self.filename = filename
+        self.subtitles = subtitles
+        self.left_adjective, self.right_adjective = (
+            filename_to_adjectives(filename))
+
+    def get_size(self) -> Tuple[int, int]:
+        return get_picture_size(self.filename)
+
+
+def remove_subtitles_from_double_adjective_pic(
+        pic: DoubleAdjectivePic) -> DoubleAdjectivePic:
+    if not pic.subtitles:
+        return pic
+
+    size = pic.get_size()
+    if size == (640, 402):
+        pass
+    raise Exception("Not Implemented")
+
+
+def split_double_adjective_pic(
+        pic: DoubleAdjectivePic) -> Tuple[AdjectivePic, AdjectivePic]:
+    raise Exception("Not Implemented")
+
 
 def filename_to_adjectives(filename: str) -> Tuple[str, str]:
     """
