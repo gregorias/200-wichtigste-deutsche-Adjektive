751bb347b00e5c500c86397ae81a70cce7f43a5f
Grzegorz Milka <grzegorzmilka@gmail.com>
2020-05-18 10:20:44 +0200

    Add pic manipulation utilities

--- test/test_process.py
+++ test/test_process.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from functools import partial
+from os import path
+from typing import Union
 import unittest
 
 from tools import process
+
+from PIL import Image, ImageChops
+
+PROJECT_DIR = path.dirname(path.dirname(path.realpath(__file__)))
+TESTDATA_DIR = path.join(PROJECT_DIR, 'testdata')
+# A 640x402 double picture with subs
+LEFT_RIGHT_IMG = path.join(
+    TESTDATA_DIR, "left_right_Adjektive_Deutsch_deutschlernerblog.png")
+LEFT_IMG = path.join(TESTDATA_DIR, "left.png")
+LEFT_NO_SUBS_IMG = path.join(TESTDATA_DIR, "left_no_subs.png")
+RIGHT_IMG = path.join(TESTDATA_DIR, "right.png")
+
+
+def assertTwoImagesEqual(self, a: Image, b: Image, msg=None):
+    diff = ImageChops.difference(a, b)
+    if diff.getbbox():
+        raise self.failureException(
+            "The picture images differ.\n{}".format(msg))
+
 
 class FilenameToAdjectivesTestCase(unittest.TestCase):
     def test_filename_to_adjectives_rejects_malformed_filenames(self):
@@ -11,8 +33,7 @@ class FilenameToAdjectivesTestCase(unittest.TestCase):
 
     def test_filename_to_adjectives_rejects_missing_trunk(self):
         with self.assertRaises(Exception):
-            process.filename_to_adjectives(
-                "Adjektive-deutschlernerblog.png"),
+            process.filename_to_adjectives("Adjektive-deutschlernerblog.png"),
 
     def test_filename_to_adjectives_rejects_missing_separators(self):
         with self.assertRaises(Exception):
@@ -36,3 +57,43 @@ class FilenameToAdjectivesTestCase(unittest.TestCase):
             process.filename_to_adjectives(
                 "wertvoll-wertlos-Adjektive-Bilder-Gegensatzpaare-deutschlernerblog.png"
             ), ("wertvoll", "wertlos"))
+
+    def test_filename_to_adjectives_parses_with_dirname(self):
+        self.assertEqual(
+            process.filename_to_adjectives(
+                "/a/full/path/wertvoll-wertlos-Adjektive-Bilder-Gegensatzpaare-deutschlernerblog.png"
+            ), ("wertvoll", "wertlos"))
+
+
+class PictureSizeTestCase(unittest.TestCase):
+    def test_get_picture_size_returns_correct_size(self):
+        self.assertEqual(process.get_picture_size(LEFT_RIGHT_IMG), (640, 402))
+
+
+class SubtitleRemovalTestCase(unittest.TestCase):
+    def test_remove_subs(self):
+        with Image.open(LEFT_IMG) as left_img, \
+                Image.open(LEFT_NO_SUBS_IMG) as left_no_subs_img:
+            assertTwoImagesEqual(self, process.remove_subs(left_img),
+                                 left_no_subs_img)
+
+
+class SplitTestCase(unittest.TestCase):
+    def test_split_double_adjective_img(self):
+        with Image.open(LEFT_IMG) as left_img, \
+              Image.open(RIGHT_IMG) as right_img, \
+              Image.open(LEFT_RIGHT_IMG) as left_right_img:
+            l, r = process.split_double_adjective_img(left_right_img)
+            assertTwoImagesEqual(self, l, left_img)
+            assertTwoImagesEqual(self, r, right_img)
+
+    def test_split_double_adjective_pic(self):
+        dap = process.DoubleAdjectivePic.from_original(LEFT_RIGHT_IMG,
+                                                       subs=True)
+        expected_left_pic = process.SingleAdjectivePic(LEFT_IMG,
+                                                       adjective='left',
+                                                       subs=True)
+        expected_right_pic = process.SingleAdjectivePic(RIGHT_IMG,
+                                                        adjective='right',
+                                                        subs=True)
+        self.assertEqual(dap.split(), (expected_left_pic, expected_right_pic))
--- tools/process.py
+++ tools/process.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """A collection of utilities for processing scraped images."""
-from typing import Tuple
+import abc
+from contextlib import closing
+from os import path
+import tempfile
+from typing import Tuple, Union
+
+from PIL import Image, ImageChops
 
 
 def filename_to_adjectives(filename: str) -> Tuple[str, str]:
@@ -9,6 +15,7 @@ def filename_to_adjectives(filename: str) -> Tuple[str, str]:
     >>> filename_to_adjectives("wild_zahm_Adjektive_Deutsch_deutschlernerblog.png")
     ("wild", "zahm")
     """
+    filename = path.basename(filename)
     adj_idx = filename.find("Adjektive")
     if adj_idx == -1:
         raise Exception(
@@ -46,3 +53,156 @@ def filename_to_adjectives(filename: str) -> Tuple[str, str]:
     raise Exception("When given three words, expected the second of them to " +
                     "start with nicht, but got: " + str(words) +
                     ". Add a handler " + "to this function")
+
+
+def get_picture_size(pic_filename: str) -> Tuple[int, int]:
+    with Image.open(pic_filename) as im:
+        return im.size
+
+
+def remove_subs(pic: Image) -> Image:
+    size = pic.size
+    if size[1] == 402:
+        return pic.crop((0, 0, size[0], 330))
+    if size[1] == 803:
+        return pic.crop((0, 0, size[0], 660))
+    raise Exception("Not Implemented")
+
+
+def split_double_adjective_img(
+        pic: Image.Image) -> Tuple[Image.Image, Image.Image]:
+    size = pic.size
+    return (
+        pic.crop((0, 0, pic.size[0] // 2, size[1])),
+        pic.crop((pic.size[0] // 2, 0, pic.size[0], size[1])),
+    )
+
+
+class AdjectivePic(abc.ABC):
+    @abc.abstractmethod
+    def get_size(self) -> Tuple[int, int]:
+        pass
+
+    @abc.abstractmethod
+    def get_filename(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    def remove_subs(self):
+        pass
+
+    def to_image(self) -> Image.Image:
+        return Image.open(self.get_filename())
+
+    __hash__ = None  # type: ignore
+
+
+class SingleAdjectivePic(AdjectivePic):
+    def __init__(self, filename: str, adjective: str, subs: bool):
+        self.filename = filename
+        self.adjective = adjective
+        self.subs = subs
+
+    @staticmethod
+    def from_image(img: Image, adjective: str,
+                   subs: bool) -> 'SingleAdjectivePic':
+        with tempfile.NamedTemporaryFile(delete=False,
+                                         prefix=adjective +
+                                         ('_sub' if subs else '') + '_',
+                                         suffix='.png') as tmp:
+            img.save(tmp)
+            filename = tmp.name
+        return SingleAdjectivePic(filename, adjective, subs)
+
+    def get_size(self) -> Tuple[int, int]:
+        return get_picture_size(self.filename)
+
+    def get_filename(self) -> str:
+        return self.filename
+
+    def remove_subs(self) -> AdjectivePic:
+        if not self.subs:
+            return self
+        with self.to_image() as img:
+            no_sub_img = remove_subs(img)
+        return SingleAdjectivePic.from_image(no_sub_img,
+                                             adjective=self.adjective,
+                                             subs=False)
+
+    def __eq__(self, other):
+        with self.to_image() as img,\
+                other.to_image() as other_img:
+            diff = ImageChops.difference(img, other_img)
+            if diff.getbbox():
+                return True
+
+        return (self.adjective == other.adjective
+                and self.adjective == other.adjective
+                and self.subs == other.subs)
+
+
+class DoubleAdjectivePic(AdjectivePic):
+    def __init__(self, filename: str, left_adjective: str,
+                 right_adjective: str, subs: bool):
+        self.filename = filename
+        self.subs = subs
+        self.left_adjective, self.right_adjective = (left_adjective,
+                                                     right_adjective)
+
+    @staticmethod
+    def from_original(filename: str, subs: bool):
+        left_adjective, right_adjective = (filename_to_adjectives(filename))
+        return DoubleAdjectivePic(filename, left_adjective, right_adjective,
+                                  subs)
+
+    @staticmethod
+    def from_image(img: Image, left_adjective: str, right_adjective: str,
+                   subs: bool) -> 'DoubleAdjectivePic':
+        with tempfile.NamedTemporaryFile(delete=False,
+                                         prefix=left_adjective + '_' +
+                                         right_adjective + '_' +
+                                         ('_sub' if subs else '') + '_',
+                                         suffix='.png') as tmp:
+            img.save(tmp)
+            filename = tmp.name
+        return DoubleAdjectivePic(filename, left_adjective, right_adjective,
+                                  subs)
+
+    def get_size(self) -> Tuple[int, int]:
+        return get_picture_size(self.filename)
+
+    def get_filename(self) -> str:
+        return self.filename
+
+    def split(self) -> Tuple[SingleAdjectivePic, SingleAdjectivePic]:
+        with Image.open(self.filename) as img:
+            left, right = split_double_adjective_img(img)
+            with closing(left) as left, closing(right) as right:
+                return (SingleAdjectivePic.from_image(left,
+                                                      self.left_adjective,
+                                                      self.subs),
+                        SingleAdjectivePic.from_image(right,
+                                                      self.right_adjective,
+                                                      self.subs))
+
+    def remove_subs(self) -> 'DoubleAdjectivePic':
+        if not self.subs:
+            return self
+        with self.to_image() as img:
+            no_sub_img = remove_subs(img)
+        return DoubleAdjectivePic.from_image(
+            no_sub_img,
+            left_adjective=self.left_adjective,
+            right_adjective=self.right_adjective,
+            subs=False)
+
+    def __eq__(self, other):
+        with self.to_image() as img,\
+                other.to_image() as other_img:
+            diff = ImageChops.difference(img, other_img)
+            if diff.getbbox():
+                return True
+
+        return (self.left_adjective == other.left_adjective
+                and self.right_adjective == other.right_adjective
+                and self.subs == other.subs)
